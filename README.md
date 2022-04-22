f7191a10b38fe18aba810672b167e4c9fa200c86
Mateusz-Ramotowski-Poland <83215700+Mateusz-Ramotowski-Poland@users.noreply.github.com>
2022-04-22 11:49:13 +0200

    I added README.md file and change pin input to password type

---
+++ README.md
@@ -0,0 +1,36 @@
+# Bankist app
+Live demo: [_here_](https://mateusz-ramotowski-poland.github.io/Bankist-App-Udemy-Course/).
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Project Status](#project-status)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+
+## General Information
+This is a web application that was inside the course: "The Complete JavaScript Course 2022: From Zero to Expert!" by Jonas Schmedtmann. I made it for educational purposes. 
+
+## Technologies Used
+Project is created with:
+* HTML5
+* CSS3
+* JS(ES6+)
+
+I was using Chrome developer tools for testing this application.
+## Features
+* fake login
+* transfer money to another account
+* request loan
+* delete account
+* sort movements
+
+## Project Status
+Finished project.
+
+## Acknowledgements
+Thanks [_Jonas Schmedtmann_](https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648873#overview) for great JavaScript course!
+
+## Contact
+mateusz.ramotowski.praca@gmail.com - feel free to contact me!
--- script.js
+++ script.js
@@ -79,7 +79,7 @@ const displayMovements = function (movements, sort = false) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}€</div>
+      <div class="movements__value">${mov}&#8364;</div>
     </div>
       `;
 
@@ -89,19 +89,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}&#8364;`; //character entity reference for euro sign
+  labelBalance.textContent = `${acc.balance}€`; 
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}&#8364;`; //character entity reference for euro sign
+  labelSumIn.textContent = `${incomes}€`; 
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}&#8364;`; //character entity reference for euro sign
+  labelSumOut.textContent = `${Math.abs(out)}€`; 
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -111,7 +111,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}&#8364;`; //character entity reference for euro sign
+  labelSumInterest.textContent = `${interest}€`; 
 };
 
 const createUsernames = function (accs) {
